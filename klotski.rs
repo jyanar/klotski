113f119e08d1b1ec67e6b17ae277c369afce4be7
Jorge Yanar <jorgeyanar@gmail.com>
2020-06-24 00:54:47 -0400

    rust solution working, just need to implement remove_cycles

--- klotski.rs
+++ klotski.rs
@@ -5,11 +5,8 @@
 // Author: Jorge Yanar
 //
 
-//use std::io;
-//use std::cmp::Ordering;
-//use rand::Rng;
-use rand::seq::SliceRandom;
 use rand::Rng;
+use rand::seq::SliceRandom;
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
@@ -17,60 +14,39 @@ use itertools::Itertools;
 
 use std::any::type_name;
 
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
 
 fn main() {
-    let solns: Vec<(i32,usize,usize)> = vec![(4,3,1), (4,1,3)];
+    let solns: Vec<(i32,usize,usize)> = vec![(4,3,1)];
     let board: [[i32; 4]; 5] = [
         [2, 4, 4, 2],
         [2, 4, 4, 2],
         [2, 3, 3, 2],
-        [2, 0, 0, 2],
-        [1, 1, 1, 1],
+        [2, 1, 1, 2],
+        [1, 0, 0, 1],
     ];
-    print_board(board);
+    println!("Starting board: ");
+    print_board(&board);
+    let (states_seq, nvisited) = find_solution_path(board, solns);
+    println!("Number of states visited: {}", nvisited);
+    println!("Final board state: ");
+    print_board(&states_seq.last().unwrap());
+}
 
-    let ss = randomwalk(board, 10);
-    for i in 0 .. ss.len() {
-        print_board(ss[i]);
-    }
 
-    // let nstates = get_next_states(board);
-    // for i in 0 .. nstates.len() {
-    //     print_board(nstates[i]);
-    //     let ps = get_board_tuple_repr(nstates[i]);
-    // }
-    
-
-    // let x = rand::thread_rng().gen_range(0, 4);
-    // println!("{}", x);
-    // let (states_seq, nvisited) = find_solution_path(board, solns);
-
-    // let nstates = get_next_states(board);
-    // for i in 0 .. nstates.len() {
-    //     print_board(nstates[i]);
-    //     let ps = get_board_tuple_repr(nstates[i]);
-    //     for j in 0 .. ps.len() {
-    //         println!("\t{} {} {}", ps[j].0, ps[j].1, ps[j].2);
-    //     }
-    //     println!("");
-    // }
+fn take_step(board: [[i32;4];5]) -> [[i32;4];5] {
+    let next_states = get_next_states(board);
+    next_states[rand::thread_rng().gen_range(0, next_states.len())]
 }
 
 
 fn randomwalk(board: [[i32; 4]; 5], n_steps: usize) -> Vec<[[i32; 4]; 5]> {
     let mut states_seq: Vec<[[i32; 4]; 5]> = vec![board];
     for i in 0 .. n_steps {
-        let next_states = get_next_states(board);
-        let board = next_states[rand::thread_rng().gen_range(0, next_states.len())];
-        states_seq.push(board);
+        // let board = take_step(board);
+        states_seq.push(take_step(states_seq[states_seq.len()-1]));
     }
     return states_seq;
 }
-
-
 
 
 /// Runs brute-force search on a board to find a path from the given board
@@ -79,55 +55,46 @@ fn find_solution_path(
     board: [[i32; 4]; 5],
     solns: Vec<(i32,usize,usize)>
 ) -> (Vec<[[i32; 4]; 5]>, usize) {
-
-    // Keep track of number of visited states and a sequential path of all the
-    // boards visited.
     let mut nvisited: usize = 1;
-    let mut soln_found: bool = false;
     let mut states_seq: Vec<[[i32; 4]; 5]> = vec![board];
 
-    // While we've not found a board state containing a piece specified in the
-    // solns vec, continue a random walk through the klotski space.
     loop {
-        let next_states = get_next_states(board);
+        let next_states = get_next_states(states_seq.last().unwrap().clone());
         for i in 0 .. next_states.len() {
-            // check 
-            let ps = get_board_tuple_repr(next_states[i]);
-            for j in 0 .. ps.len() {
-                if solns.contains(&ps[j]) {
-                    soln_found = true;
-                }
-            }
-
-            if soln_found == true {
+            let ps = get_board_tuple_repr(&next_states[i]);
+            let soln_found = ps.iter().any(|&p| solns.contains(&p));
+            if soln_found {
                 println!("Found solution state!");
-                states_seq.push(next_states[i]);
+                states_seq.push(next_states[i].clone());
                 nvisited += 1;
-                states_seq = remove_cycles(states_seq);
+                // states_seq = remove_cycles(states_seq);
                 return (states_seq, nvisited);
             }
-
         }
-        let board = next_states[rand::thread_rng().gen_range(0, next_states.len())];
-        states_seq.push(board);
+        states_seq.push(next_states[rand::thread_rng().gen_range(0, next_states.len())].clone());
         nvisited += 1;
         if nvisited % 10000 == 0 {
             println!("States visited: {}", nvisited);
-            print_board(states_seq[states_seq.len()-1]);
             // states_seq = remove_cycles(states_seq);
         }
     }
 }
+
+
+/// Removes cycles from a given sequence of states
+fn remove_cycles(states_seq: Vec<[[i32; 4]; 5]>) -> Vec<[[i32; 4]; 5]> {
+    return states_seq;
+}
+
+
 // fn hash_states_seq(states_seq: Vec<[[i32;4];5]>) -> Vec<u64> {
 //     let mut hasher = DefaultHasher::new();
 //     let sh = 
-    
-
 // }
 
 
 /// Prints the board to stdout
-fn print_board(board: [[i32; 4]; 5]) {
+fn print_board(board: &[[i32; 4]; 5]) {
     println!();
     for i in 0..5 {
         print!("\t");
@@ -155,20 +122,24 @@ fn get_next_states(board: [[i32; 4]; 5]) -> Vec<[[i32; 4]; 5]> {
     let mut pieces: Vec<(i32,usize,usize)> = vec![];
     for (i,j) in zeros_idx.into_iter() {
         if i != 0 && board[i-1][j] != 0 {
-            pieces.push(get_board_piece_at_ij(board, i-1, j));
+            pieces.push(get_board_piece_at_ij(&board, i-1, j));
         }
         if i != 4 && board[i+1][j] != 0 {
-            pieces.push(get_board_piece_at_ij(board, i+1, j));
+            pieces.push(get_board_piece_at_ij(&board, i+1, j));
         }
         if j != 0 && board[i][j-1] != 0 {
-            pieces.push(get_board_piece_at_ij(board, i, j-1));
+            pieces.push(get_board_piece_at_ij(&board, i, j-1));
         }
         if j != 3 && board[i][j+1] != 0 {
-            pieces.push(get_board_piece_at_ij(board, i, j+1));
+            pieces.push(get_board_piece_at_ij(&board, i, j+1));
         }
     }
-    let pieces: Vec<_> = pieces.into_iter().unique().collect();
-
+    // Remove duplicate pieces, as well as "empty" pieces
+    let mut pieces: Vec<_> = pieces.into_iter().unique().collect();
+    // for p in 0 .. pieces.len() {
+    //     println!("{}", pieces[p].0);
+    // }
+    pieces.retain(|&p| p.0 != 0);
 
     // Check if any of the identified pieces can move. If so, add resulting
     // board state to the list of possible future states
@@ -195,49 +166,45 @@ fn get_next_states(board: [[i32; 4]; 5]) -> Vec<[[i32; 4]; 5]> {
 }
 
 
-/// Returns the board piece, in (i32, i32, i32) form, located
+/// Returns the board piece, in (i32, usize, usize) form, located
 /// at index i, j
 fn get_board_piece_at_ij(
-    board: [[i32; 4]; 5],
+    board: &[[i32; 4]; 5],
     i: usize,
     j: usize,
 ) -> (i32,usize,usize) {
-    if board[i][j] == 0 {
-        return (0, i, j);
-    } else if board[i][j] == 1 {
-        return (1, i, j);
-    } else if board[i][j] == 2 {
-        match i {
+    match board[i][j] {
+        // Pieces
+        0 => return (0, i, j),
+        1 => return (1, i, j),
+        2 => match i {
             0 => return (2, i, j),
             4 => return (2, i-1, j),
             _ => {
-                let ps = get_board_pieces_of_type(board, 2);
-                for p in 0 .. ps.len() {
-                    let pi: i32 = ps[p].1 as i32;
-                    let pj: i32 = ps[p].2 as i32;
-                    if pi == i as i32 {
+                let candidates: Vec<(i32,usize,usize)> = get_board_pieces_of_type(&board, 2)
+                                                                    .into_iter()
+                                                                    .filter(|&p| p.2 == j)
+                                                                    .collect();
+                for p in 0 .. candidates.len() {
+                    let pi = candidates[p].1; let pj = candidates[p].2;
+                    if pi == i {
                         return (2, i, j);
-                    } else if pi + 1 == i as i32 {
-                        return (2, i-1, j);
-                    } else if pi - 1 == i as i32{
+                    } else if pi + 1 == i {
+                        return (2, (i as i32 - 1 as i32) as usize, j);
+                    } else if (pi as i32 - 1 as i32) as usize == i {
                         return (2, i+1, j);
-                    } else {
-                        continue;
                     }
                 }
+                panic!("2 piece not found!");
             },
-        };
-        return (0, 0 as usize, 0 as usize);
-    } else if board[i][j] == 3 {
-        match j {
+        },
+        3 => match j {
             0 => return (3, i, j),
             3 => return (3, i, j-1),
-            _ => return get_board_pieces_of_type(board, 3)[0],
-        };
-    } else if board[i][j] == 4 {
-        return get_board_pieces_of_type(board, 4)[0];
-    } else {
-        return (0, 0 as usize, 0 as usize);
+            _ => return get_board_pieces_of_type(&board, 3)[0],
+        },
+        4 => return get_board_pieces_of_type(&board, 4)[0],
+        _ => panic!("piece not found!"),
     }
 }
 
@@ -302,7 +269,6 @@ fn is_valid_move(
             _ => (),
         },
         _ => (),
-
     };
     return false;
 }
@@ -314,7 +280,7 @@ fn move_piece(
     dir:   (i32,i32),
 ) -> [[i32; 4]; 5] {
 
-    let mut board = board;
+    let mut board = board.clone();
     let p = piece.0;
     let i = piece.1;
     let j = piece.2;
@@ -359,11 +325,11 @@ fn move_piece(
 }
 
 
-fn get_board_tuple_repr(board: [[i32; 4]; 5]) -> Vec<(i32,usize,usize)> {
-    let mut board = board;
+fn get_board_tuple_repr(board: &[[i32; 4]; 5]) -> Vec<(i32,usize,usize)> {
+    let mut board = board.clone();
     let mut pieces: Vec<(i32,usize,usize)> = vec![];
-    for i in 0..5 {
-        for j in 0..4 {
+    for i in 0 .. 5 {
+        for j in 0 .. 4 {
             match board[i][j] {
                 1 => pieces.push((1,i,j)),
                 2 => {
@@ -392,10 +358,10 @@ fn get_board_tuple_repr(board: [[i32; 4]; 5]) -> Vec<(i32,usize,usize)> {
 /// Returns list of all pieces on the board of specified type, in tuple
 /// representation (piecetype: i32, row: usize, col: usize)
 fn get_board_pieces_of_type(
-    board: [[i32; 4]; 5],
+    board: &[[i32; 4]; 5],
     piece_type: i32
 ) -> Vec<(i32,usize,usize)> {
-    let mut board = board;
+    let mut board = board.clone();
     let mut pieces: Vec<(i32,usize,usize)> = vec![];
     for i in 0..5 {
         for j in 0..4 {
@@ -417,61 +383,8 @@ fn get_board_pieces_of_type(
     return pieces;
 }
 
-/// Removes cycles from a given sequence of states
-fn remove_cycles(states_seq: Vec<[[i32; 4]; 5]>) -> Vec<[[i32; 4]; 5]> {
-    return states_seq;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
 
-
-
-
-// Code for checking is_valid_move using a match piece-first approach
-
-    // // Otherwise, check more specific movements
-    // match p {
-    //     // Single square
-    //     1 => return board[(i as i32 + di) as usize][(j as i32 + dj) as usize] == 0,
-    //     // Vertical rectangle
-    //     2 => {
-    //         // Up / Down
-    //         match di {
-    //             -1 => return board[i-1][j] == 0,
-    //              1 => return board[i+2][j] == 0,
-    //              _ => (),
-    //         };
-    //         // Left / Right
-    //         match dj {
-    //             -1 => return board[i][j-1] == 0 && board[i+1][j-1] == 0,
-    //              1 => return board[i][j+1] == 0 && board[i+1][j+1] == 0,
-    //              _ => (),
-    //         };
-    //     },
-    //     // Horizontal rectangle
-    //     3 => {
-    //         // Up/Down
-    //         match di {
-    //             -1 => return board[i-1][j] == 0 && board[i-1][j+1] == 0, // Up
-    //              1 => return board[i+1][j] == 0 && board[i+1][j+1] == 0, // Down
-    //              _ => (),
-    //         };
-    //         // Left/Right
-    //         match dj {
-    //             -1 => return board[i][j-1] == 0, // Left
-    //              1 => return board[i][j+2] == 0, // Right
-    //              _ => (),
-    //         };
-    //     },
-    //     // Large square block
-    //     4 => {
-    //         match di { // UP / DOWN
-    //             -1 => return board[i-1][j] == 0 && board[i-1][j+1] == 0 , // UP
-    //              1 => return board[i+2][j] == 0 && board[i+2][j+1] == 0 , // DOWN
-    //              _ => (),
-    //         };
-    //         match dj { // LEFT / RIGHT
-    //             -1 => return board[i][j-1] == 0 && board[i+1][j-1] == 0, // LEFT
-    //              1 => return board[i][j+2] == 0 && board[i+1][j+2] == 0, // RIGHT
-    //              _ => (),
-    //         };
-    //     },
-    // };
